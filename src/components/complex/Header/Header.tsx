833f0dc2e42c6ebbf3a62e7406eb082aa5a2b435
fledageo <dav.gevorgyan.main@gmail.com>
2024-08-07 14:56:54 +0400

    welcome message text

--- src/components/complex/Header/Header.tsx
+++ src/components/complex/Header/Header.tsx
@@ -5,13 +5,14 @@ import Link from 'next/link';
 import { LuBookOpenCheck } from "react-icons/lu";
 import { IoPerson } from "react-icons/io5";
 const Header = () => {
+  console.log(window.innerWidth)
   return (
     <>
       <div className={style.container}>
         <div className={style.mainWrapper}>
           <nav className={`navbar ${style.navWrapper}`}>
             <div className={style.logoBlock}>
-              <Link href={"/"} className={`navbar-brand m-0 ${style.menuLink}`}><LuBookOpenCheck size={40} /></Link>
+              <Link href={"/"} className={`navbar-brand m-0 ${style.menuLink}`}><LuBookOpenCheck size={40} className={style.logoIcon}/></Link>
             </div>
             <div className={style.menuBlock}>
               <ul className={`navbar-nav ${style.navMenu}`}>
@@ -31,7 +32,7 @@ const Header = () => {
             </div>
             <div className={style.othersBlock}>
               {/* need to change */}
-              <Link href={"/"} className={`navbar-brand m-0 ${style.menuLink}`}><IoPerson size={25}/></Link>
+              <Link href={"/"} className={`navbar-brand m-0 ${style.menuLink}`}><IoPerson size={30} className={style.personIcon}/></Link>
             </div>
           </nav>
         </div>
--- src/components/complex/Welcome-section/WelcomeSection.tsx
+++ src/components/complex/Welcome-section/WelcomeSection.tsx
@@ -4,45 +4,53 @@ import style from "./WelcomeSection.module.css"
 
 
 const WelcomeSection = () => {
-  const [isVisible,setIsVisible] = useState<Boolean>(false)
+  const [isVisible, setIsVisible] = useState<Boolean>(false)
   const componentRef = useRef(null)
-  
+
   console.log("rerender")
-  const onIntersection = ([entries]:IntersectionObserverEntry[]) => {
+  const onIntersection = ([entries]: IntersectionObserverEntry[]) => {
     setIsVisible(entries.isIntersecting)
   }
   const options = {
-    root: null, 
+    root: null,
     rootMargin: '0px',
-    threshold: 0.5, 
+    threshold: 0.5,
   }
 
   useEffect(() => {
-    const observer = new IntersectionObserver(onIntersection,options)
+    const observer = new IntersectionObserver(onIntersection, options)
     if (componentRef.current) {
       observer.observe(componentRef.current);
     }
-  },[])
+  }, [])
 
   return (
     <>
       <div className={style.container} ref={componentRef} >
         <div className={style.mainWrapper}>
           <div className={style.leftColumn}>
-
+            <div className={style.welcomeMessageBlock}>
+              <p className={style.welcomeText}>
+                We're excited to help you organize your day and achieve your goals.
+                Whether you're managing your work schedule, planning personal activities,
+                or setting reminders for important tasks, DayPlanner is here to streamline your daily routine.
+              </p>
+              <div className={style.welcomActionBlock}>
+                <button className={`btn btn-outline-light`}>Get Started</button>
+              </div>
+            </div>
           </div>
           <div className={style.rightColumn}>
-            <div className={style.welcomeMessageBlock}>
+            <div>
               {
                 isVisible ? <div className={style.welcomeTitleBlock}>
-                <span className={style.title}>Plan...</span>   
-                <span className={style.title}>Achieve...</span>   
-                <span className={style.title}>Succeed...</span>
-              </div> : ""
+                  <span className={style.title}>Plan...</span>
+                  <span className={style.title}>Achieve...</span>
+                  <span className={style.title}>Succeed...</span>
+                </div> : ""
               }
             </div>
           </div>
-          {/* <h1 style={{color:"white"}}>Plan. Achieve. Succeed.</h1> */}
         </div>
       </div>
     </>
